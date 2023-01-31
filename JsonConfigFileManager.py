1dcbdb34dba494f1557b43f2b5fd05f0e062960e
suffolk066 <zofldj0913@naver.com>
2023-01-31 19:27:40 +0900

    first commit

---
+++ JsonConfigFileManager.py
@@ -0,0 +1,35 @@
+import json
+from easydict import EasyDict
+
+class JsonConfigFileManager:
+    def __init__(self, file_path):
+        self.values = EasyDict()
+        if file_path:
+            self.file_path = file_path
+            self.reload()
+
+    def reload(self):
+        self.clear()
+        if self.file_path:
+            with open(self.file_path, 'r') as f:
+                self.values.update(json.load(f))
+
+    def clear(self):
+        self.values.clear()
+                
+    def update(self, in_dict):
+        for (k1, v1) in in_dict.items():
+            if isinstance(v1, dict):
+                for (k2, v2) in v1.items():
+                    if isinstance(v2, dict):
+                        for (k3, v3) in v2.items():
+                            self.values[k1][k2][k3] = v3
+                    else:
+                        self.values[k1][k2] = v2
+            else:
+                self.values[k1] = v1     
+            
+    def export(self, save_file_name):
+        if save_file_name:
+            with open(save_file_name, 'w') as f:
+                json.dump(dict(self.values), f)
